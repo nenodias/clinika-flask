e7d7542f2101f775672f4c7c2b83f87674664822
Horácio Dias <horacio.dias92@gmail.com>
2016-11-12 23:56:06 -0200

    Adicionando o blueprint de paciente

--- app/controller/paciente_blueprint.py
+++ app/controller/paciente_blueprint.py
@@ -1,83 +1,139 @@
 # -*- coding: utf-8 -*-
+import json
+from pdb import set_trace
 from flask import (Blueprint, render_template, request, redirect, url_for, flash, 
-    jsonify, render_template)
-from app import db
+    jsonify, render_template, Response)
+from app.db import db, pacientes as _table
 
 paciente_blueprint = Blueprint('paciente', __name__)
 
 @paciente_blueprint.route('/')
-def ajax_list():
+def index():
+    contexto = {}
+    _nome = request.args.get('nome', '')
+    _cpf = request.args.get('cpf', '')
+    contexto['model'] = {
+        'nome':_nome,
+        'cpf':_cpf
+    }
+    return render_template('paciente/consulta.html', **contexto)
+
+@paciente_blueprint.route('/form/', defaults={'pk':None}, methods = ['post', 'get'])
+@paciente_blueprint.route('/form/<pk>', methods = ['post', 'get'])
+def form(pk):
+    #Pega os dados dos campos na tela
+    contexto = {}
+    contexto['model'] = {}
+    contexto['tupla_plano'] = ( 
+        ('', 'Selecionar'),
+        ('SEM_COBERTURA', 'Sem Cobertura'),
+        ('COBERTURA_PARCIAL', 'Cobertura parcial')
+        ('COBERTURA_TOTAL', 'Cobertura total')
+    )
+    if request.method == 'POST':
+        #Pega os dados dos campos na tela
+        nome = request.form.get("nome")
+        cpf = int(request.form.get("cpf"))
+        telefone = request.form.get("telefone")
+        plano = request.form.get("plano")
+        rua = request.form.get("rua")
+        numero = request.form.get("numero")
+        bairro = request.form.get("bairro")
+        cidade = request.form.get("cidade")
+        estado = request.form.get("estado")
+      
+        #Criar dicionário com os dados
+        dicionario = {
+            "nome":nome,
+            "cpf":cpf,
+            "telefone": telefone,
+            "plano": plano,
+            "rua": rua,
+            "numero": numero,
+            "bairro": bairro,
+            "cidade": cidade,
+            "estado": estado
+        }
+        mensagem = None
+        try:
+            if pk:
+                dicionario['id'] = pk
+                id_cadastro = _table.update(dicionario,['id'])
+                contexto['mensagem'] = u'Paciente {0} atualizado com sucesso'
+            else:
+                id_cadastro = _table.insert(dicionario,['id'])
+                contexto['mensagem'] = u'Paciente {0} cadastrado com sucesso'
+        except:
+            contexto['mensagem'] = u'Erro ao cadastrar paciente'
+    elif pk:
+        data = _table.find_one(id=pk)
+        contexto['model'] = dict(data)
+    return render_template('paciente/cadastro.html', **contexto)
+
+
+@paciente_blueprint.route('/delete/<pk>', methods = ['post'])
+def delete(pk):
+    data = _table.find_one(id=pk)
+    if data:
+        if _table.delete(id=pk):
+            return '', 200
+    return '',404
+
+@paciente_blueprint.route('/ajax', methods = ['get'])
+def ajax():
     _limit = int(request.args.get('limit','10'))
     _offset = int(request.args.get('offset','0'))
-    _criteria = request.args.get('search', '')
-    _value = request.args.get('value', '')
-    _table = db.pacientes
-    
-    count = _table.count()
-    total_pages = count // _limit;
-    page = _offset + 1;
+    _nome = request.args.get('nome', '')
+    _cpf = request.args.get('cpf', '')
     items = []
+    params = {}
+    sql = 'SELECT * FROM {0} WHERE 1 = 1 '.format(_table.table.name)
+    if _nome:
+        params['nome'] = '%'+_nome+'%'
+        sql += ' AND nome like :nome '
+    if _cpf:
+        params['cpf'] = int(_cpf)
+        sql += ' AND cpf = :cpf '
+    
+    sql += ' LIMIT :offset,:limit'
+    params['offset'] = _offset
+    params['limit'] = _limit
 
-    search = {
-        '_limit':_limit,
-        '_offset':_offset,
-    }
-    number_results = 0
-    if _criteria and _value:
-        search[_criteria] = _value
     try:
-        for item in _table.find(**search):
-            items.append( item )
-            number_results += 1
+        fetch = db.engine.execute(sql, params)
+        colunas = fetch.keys()
+        for dado in fetch:
+            items.append( dict(zip(colunas, dado)) )
     except Exception as ex:
         print(ex)
-    data = {
-        'page':page,
-        'total_pages':total_pages,
-        'results': number_results,
-        'total':count,
-        'data':items
-    }
-    return jsonify( data )
+    return Response(response=json.dumps( items ), status=200, mimetype="application/json")
 
-@paciente_blueprint.route('/', methods=['POST'])
-def ajax_post():
-    _table = db.pacientes
-    data = request.form.to_dict()
-    if 'id' in data:
-        operation = _table.update(data,['id'])
-    else:
-        operation = _table.insert(update,['id'])
-    if operation:
-        data = _table.find_one(id=id)
-        return jsonify(data), 200
-    return '',404
+@paciente_blueprint.route('/count', methods = ['get'])
+def count():
+    _nome = request.args.get('nome', '')
+    _cpf = request.args.get('cpf', '')
+    items = []
+    params = {}
+    sql = 'SELECT COUNT(1) as count FROM {0} WHERE 1 = 1 '.format(_table.table.name)
+    if _nome:
+        params['nome'] = '%'+_nome+'%'
+        sql += ' AND nome like :nome '
+    if _cpf:
+        params['cpf'] = int(_cpf)
+        sql += ' AND cpf = :cpf '
+    try:
+        fetch = db.engine.execute(sql, params)
+        colunas = fetch.keys()
+        for dado in fetch:
+            item = dict(zip(colunas, dado))
+            items.append( item )
+    except Exception as ex:
+        print(ex)
+    return Response(response=json.dumps( items[0] ), status=200, mimetype="application/json")
 
-@paciente_blueprint.route('/<id>', methods=['GET'])
-def ajax_get(id):
-    _table = db.pacientes
-    data = _table.find_one(id=id)
+@paciente_blueprint.route('/ajax/<pk>', methods = ['get'])
+def ajax_by_id(pk):
+    data = _table.find_one(id=pk)
     if data:
-        return jsonify(data), 200
-    return '',404
-
-@paciente_blueprint.route('/<id>', methods=['PUT'])
-def ajax_put(id):
-    _table = db.pacientes
-    data = _table.find_one(id=id)
-    update = request.form.to_dict()
-    update['id'] = id
-    if data:
-        _table.update(update,['id'])
-        data = _table.find_one(id=id)
-        return jsonify(data), 200
-    return '',404
-
-@paciente_blueprint.route('/<id>', methods=['DELETE'])
-def ajax_delete(id):
-    _table = db.pacientes
-    data = _table.find_one(id=id)
-    if data:
-        if _table.delete(id=id):
-            return '', 200
+        return Response(response=json.dumps( data ), status=200, mimetype="application/json")
     return '',404
